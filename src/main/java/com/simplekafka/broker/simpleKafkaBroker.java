e0381a6f5defde00a45ad7855ed6db185b373f36
RisheDubs <rishekeshris@gmail.com>
2026-03-21 20:28:27 +1100

    Bug fixes

--- src/main/java/com/simplekafka/broker/simpleKafkaBroker.java
+++ src/main/java/com/simplekafka/broker/simpleKafkaBroker.java
@@ -8,9 +8,6 @@
 
 package com.simplekafka.broker;
 
-import java.util.List;
-
-
 public class SimpleKafkaBroker{
 
     private final int brokerId;
@@ -19,67 +16,50 @@ public class SimpleKafkaBroker{
 
     private ZooKeeperClient zkClient;
 
-    public SimpleKafkaBroker(int brokerId, String host, int port){
+    public SimpleKafkaBroker(int brokerId, String host, int port) {
         this.brokerId = brokerId;
         this.host = host;
         this.port = port;
     }
 
-    public void start() throws Exception{
+    public void start() throws Exception {
         zkClient = new ZooKeeperClient();
         zkClient.connect();
 
         registerBroker();
-        watchBroker();
+        watchBrokers();
         electController();
-        watchChildren();
     }
 
-    //Registering broker
-    private void registerBroker() throws Exception{
+    // Register broker in ZooKeeper
+    private void registerBroker() throws Exception {
         String path = "/brokers/" + brokerId;
         String data = host + ":" + port;
 
-        zkClient.createEphemeralNode(path,data);
-        System.out.println("Broker registered: "+ path);
+        zkClient.createEphemeralNode(path, data);
+        System.out.println("Broker registered: " + path);
     }
 
-    public interface ChildrenCallback {
-        void onChildrenChanged(List<String> children);
-    }
-
-    // Watching child nodes
-    public void watchChildren(String path, ChildrenCallback callback) {
-        try {
-            List<String> children = zooKeeper.getChildren(path, event -> {
-                if (event.getType() == Watcher.Event.EventType.NodeChildrenChanged) {
-                    watchChildren(path, callback); // re-register watcher
-                }
-            });
-
-            callback.onChildrenChanged(children);
-
-        } catch (Exception e) {
-            LOGGER.log(Level.SEVERE, "Failed to watch children for path: " + path, e);
-        }
-    }
-
-    //watcher for brokers
-    //this shows which brokers are alive
-    private void watchBroker(){
+    // Watch active brokers
+    private void watchBrokers() {
         zkClient.watchChildren("/brokers", children -> {
             System.out.println("Active brokers: " + children);
         });
     }
-    
-    
+
     //controller election - kafka always has one controller broker
     //logic -> Node created - you are controller
     //      -> Node exists  - Someone else is controller
     //this controller is responsible for partition assignment, Leader election, Cluster decisions
-    private void electController() throws Exception{
-        boolean isController = zkClient.createEphemeralNode("/controller", String.valueOf(brokerId));
-        if(isController){
+
+    // Controller election
+    private void electController() throws Exception {
+        boolean isController = zkClient.createEphemeralNode(
+                "/controller",
+                String.valueOf(brokerId)
+        );
+
+        if (isController) {
             System.out.println("I am the controller");
         } else {
             System.out.println("Another broker is controller");
@@ -97,6 +77,6 @@ public class SimpleKafkaBroker{
         } catch (Exception e) {
             e.printStackTrace();
         }
-    } 
+    }
 
 }
